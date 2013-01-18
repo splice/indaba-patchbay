e36a2ecd4dfe6c1af30dffc5c161e93ece8a4d0f
Steve Perkins <steve@indabamusic.com>
2013-01-18 12:08:53 -0500

    Add vendor and core files.

---
+++ public/core/dorian-http.js
@@ -0,0 +1,53 @@
+angular.module('dorian-http', [])
+.factory('http', function($http , $q , $rootScope , $cacheFactory) {
+
+  var http = {}
+  var endpoint = http.endpoint = 'https://lydian.indabamusic.com'
+
+  // get
+  http.get = function(url, config) {
+    url = endpoint + url;
+
+    config = (config || {})
+    if (http.token) {
+      config.params = (config.params || {})
+      config.params.access_token = (config.params.access_token || http.token)
+    }
+
+    return $http.get(url, config).then(onSuccess)
+
+    function onSuccess(resp) {
+      if (resp.status !== 200 || !resp.data.status || resp.data.status !== 'success') {
+        throw new Error(resp.status)
+      }
+      return resp.data.data
+    }
+
+  }
+
+
+  // post
+  http.post = function(url, dataObject, config) {
+    url = endpoint + url
+    dataObject = (dataObject || {})
+
+    config = (config || {})
+    if (http.token) {
+      config.params = (config.params || {})
+      config.params.access_token = (config.params.access_token || http.token)
+    }
+
+    return $http.post(url, dataObject, config).then(onSuccess)
+
+    function onSuccess(resp) {
+      if (resp.status !== 200) throw new Error(resp.status)
+      if (resp.data.status !== 'success') throw new Error(resp.status)
+      return resp.data.data;
+    }
+  }
+
+
+  return http
+
+
+})
---
+++ public/core/dorian-player.js
@@ -0,0 +1,47 @@
+
+angular.module('dorian-player', ['dorian-http'])
+.factory('player', ['$rootScope','http','$timeout',
+           function( $rootScope , http , $timeout) {
+
+
+  var player = {}
+
+  player.play = function(audio) {
+    if (!audio.preview_url) throw new Error("player: preview_url is required");
+    soundManager.onready(function() {
+      soundManager.stopAll()
+
+      player.sound = soundManager.createSound({
+        id: audio.id,
+        url: audio.preview_url,
+        autoLoad: true,
+        autoPlay: true
+      });
+
+      player.sound.play({
+      });
+
+    });
+  };
+
+  setInterval(function() {
+    $rootScope.$digest()
+  }, 1000)
+
+  player.stopAll = function() {
+    soundManager.stopAll()
+  }
+
+  player.isPlaying = function(audio) {
+    return player.sound
+        && player.sound.id === audio.id
+        && player.sound.playState === 1
+        && !player.sound.paused
+  }
+
+  player.position
+
+  return player;
+
+}])
+
