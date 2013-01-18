e35c0b40caa66e3151b18b7a3943607f3e932d53
Steve Perkins <steve@indabamusic.com>
2013-01-18 16:33:58 -0500

    Adds Opp service for Opp related logic

---
+++ core/Opp.js
@@ -0,0 +1,29 @@
+.factory('Opp', function(http) {
+
+  var Opp = {}
+
+
+  function inFuture(date) {
+    var now = new Date()
+    if (!(date instanceof Date)) date = new Date(date)
+    return date > now
+  }
+  function inPast(date) {
+    var now = new Date()
+    if (!(date instanceof Date)) date = new Date(date)
+    return date < now
+  }
+
+  Opp.getPhase = function(opp) {
+
+    if (inPast(opp.submission_start_date) && inFuture(opp.submission_end_date))
+      return 'submission'
+    else if (inPast(opp.voting_start_date) && inFuture(opp.voting_end_date))
+      return 'voting'
+    else if (inPast(opp.winners_date))
+      return 'winners'
+  }
+
+  return Opp
+
+})
